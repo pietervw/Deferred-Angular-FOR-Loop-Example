0e814f15b47caed886ba370717c633603efe0ba2
pietervw <pietervanwykemail@gmail.com>
2016-05-13 08:50:41 +0200

    Create example.js

---
+++ example.js
@@ -0,0 +1,27 @@
+var delayedFORLoop = function (array) {
+    var defer = $q.defer();
+
+    var loop = function (count) {
+        var item = array[count];
+
+        // Example of a promise to wait for
+        myService.DoCalculation(item).then(function (response) {
+
+        }).finally(function () {
+          // Resolve or continue with loop
+            if (count === array.length) {
+                defer.resolve();
+            } else {
+                loop(++count);
+            }
+        });
+    }
+    
+    loop(0); // Start loop
+    return defer.promise;
+}
+
+// To use:
+delayedFORLoop(array).then(function(response) {
+    // Do something
+});
